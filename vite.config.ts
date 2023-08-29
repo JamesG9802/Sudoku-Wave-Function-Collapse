7b9203ddbd67927675237a453b1b7615a69a5014
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2023-08-29 10:23:53 -0400

    Initialize Project

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [react()],
+
+  //  https://stackoverflow.com/a/66867648
+  root: 'src',
+  build: {
+    outDir: '../docs'
+  }
+})
